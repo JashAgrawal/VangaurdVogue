a42a80662c199732c9fb5155738c5b0ab4d01ab3
Jash Agrawal <agrawaljash99@gmail.com>
2023-07-07 01:48:25 +0530

    fix

---
+++ src/pages/newProducts/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="w-full h-full flex justify-evenly items-center">
+      <div className="w-1/3"></div>
+    </div>
+  );
+};
+
+export default Product;
